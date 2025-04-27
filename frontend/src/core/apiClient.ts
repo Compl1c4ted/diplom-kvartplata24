f4fda9a589304294c81eb9009b845aa86f8f780e
Compl1c4ted <danil.merkulov.09@gmail.com>
2025-04-27 12:40:03 +0400

    Added server API in the frontend

--- frontend/src/core/apiClient.ts
+++ frontend/src/core/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://diplom-kvartplata24.onrender.com/',
 });
 
 // Очередь для запросов, ожидающих обновления токена
